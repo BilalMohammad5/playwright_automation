8e104c59fbccf1545508b4c4c4a45398a7492c07
BilalMohammad5 <Bilalroshan5@gmail.com>
2022-05-10 23:27:27 +0530

    final version

--- utils/test_data.ts
+++ utils/test_data.ts
@@ -1,3 +1,9 @@
-export class Env {
-  public static url: string = "https://demoqa.com/automation-practice-form"
+export class testdata {
+  public static url: string = "https://demoqa.com/automation-practice-form";
+  public static  first_name : string = 'bilal';
+  public static  last_name :string =  'mohammad';
+  public static  email : string =  'bilalroshan5@gmail.com';
+  public static  phone : string =  '8008756992';
+  public static  student_name : string =  'BILAL MOHAMMAD'
+
 }
